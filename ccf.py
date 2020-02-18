1f1b037e00f8da4f5011d38099062a24b5cc1617
asperjay <54516471+asperjay@users.noreply.github.com>
2020-02-18 13:30:37 -0800

    basic second clicker

--- ccf.py
+++ ccf.py
@@ -1,5 +1,6 @@
 #imports/init
 import os
+import datetime
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 from pygame.locals import *
@@ -28,6 +29,8 @@ cooldownTicks=5
 #default
 clicks=0
 adder=1
+cOne=0
+totalTime=0
 #objects
 main=Renderer(screen)
 mainSq=Square(0)
@@ -37,7 +40,14 @@ mainSq=Square(0)
 squareSmall=pygame.image.load('images/square1.png')
 squareBig=pygame.image.load('images/square2.png')
 background=pygame.image.load('images/background.png')
+placeholder=pygame.image.load('images/placeholder.png')
+start = datetime.datetime.now()
 while True: #mainloop
+    stop = datetime.datetime.now()
+    result = float((stop - start).total_seconds())
+    start=stop
+    if cOne>0:
+        totalTime+=result
     screen.fill(0)
     #screen.blit
     for x in range(int(width/background.get_width()+4)):
@@ -48,6 +58,11 @@ while True: #mainloop
         main.render(squareBig, (240,160))
     else:
         main.render(squareSmall, (245,165))
+    
+    main.render(placeholder, (620,50))
+    if cOne>0 and totalTime>=1:
+        totalTime-=1
+        clicks+=cOne
     font = pygame.font.Font(None, 24)
     survivedtext = font.render(str(clicks), False, (0,0,0))
     textRect = survivedtext.get_rect()
@@ -65,4 +80,9 @@ while True: #mainloop
                 if x>245 and x<395 and y>165 and y<315:
                     mainSq.setLargeTicks(cooldownTicks)
                     clicks+=adder
+                elif x>620 and x<635 and y>50 and y<65:
+                    if clicks>=50:
+                        clicks-=50
+                        cOne+=1
+                
                 
